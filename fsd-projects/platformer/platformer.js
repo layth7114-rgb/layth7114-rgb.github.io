095f63848db501895115eacd17f419229b5b7cba
layth7114-rgb <layth7114@gmail.com>
2026-09-22 17:43:31 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -32,16 +32,21 @@ $(function () {
 
     // TODO 2 - Create Platforms
 
+createPlatform(500, 0, 20, 290);
+createPlatform(1350, 400, 50, 50, "red");
+createPlatform(100, 20, 10, 10, "lime"); // bright green for a finished platform
 
 
 
     // TODO 3 - Create Collectables
-
+createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
 
 
     
     // TODO 4 - Create Cannons
-
+createCannon("top", 200, 100);
+createCannon("right", 300, 2000);
 
     
     
